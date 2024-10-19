ea75bebc97f24116d08b3bc6b5bf24d2e272cc60
Thierry Brands <thierrybrands@hotmail.com>
2024-10-19 14:17:13 +0200

    Test reinstalling packages new virtual environment

--- training-api/resources/train_heart_predictor.py
+++ training-api/resources/train_heart_predictor.py
@@ -6,8 +6,6 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import confusion_matrix, classification_report
-import matplotlib.pyplot as plt
-import seaborn as sns
 import tensorflow as tf
 import logging
 from flask import jsonify
